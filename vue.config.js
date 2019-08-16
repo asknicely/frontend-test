d5eb1506c0c87e4f9a3b68adf5e85f35dce755ed
graemehmarker <graeme.h@markerstudio.com>
2019-08-16 17:18:16 +1200

    TASK-2: Disable index.html generation Use localised and optimised version of Bootstrap 3

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 module.exports = {
   lintOnSave: false,
   outputDir: 'web/dist',
+  // delete HTML related webpack plugins
+  chainWebpack: (config) => {
+    config.plugins.delete('html');
+    config.plugins.delete('preload');
+    config.plugins.delete('prefetch');
+  },
 };
