042b3abb2bc573c71fd4cb087d6aab419221650b
Kinson Lau <kinson@pocketsquare.co.nz>
2019-08-20 13:17:32 +1200

    TASK-EXTRA: Uninstall unused npm packages and rebuild application.

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,7 @@ Encore
    * Each entry will result in one JavaScript file (e.g. app.js)
    * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
    */
-  .addEntry("app", "./assets/vue/app.js")
-  //.addEntry('page1', './assets/js/page1.js')
-  //.addEntry('page2', './assets/js/page2.js')
+  .addEntry("app", "./assets/vue/src/app.js")
 
   // will require an extra script tag for runtime.js
   // but, you probably want this, unless you're building a single-page app
@@ -39,14 +37,5 @@ Encore
   // enables hashed filenames (e.g. app.abc123.css)
   .enableVersioning(Encore.isProduction())
   .enableVueLoader();
-
-// enables Sass/SCSS support
-//.enableSassLoader()
-
-// uncomment if you use TypeScript
-//.enableTypeScriptLoader()
-
-// uncomment if you're having problems with a jQuery plugin
-//.autoProvidejQuery()
 
 module.exports = Encore.getWebpackConfig();
