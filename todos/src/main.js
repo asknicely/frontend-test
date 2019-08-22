021b42e5bf2f9cbcf0afcc056d35f0c996bea099
wenchonghuang <huangkingsley@gmail.com>
2019-08-22 22:34:49 +1200

    # init vue project, install vue tools, vue router and vue validation

---
+++ todos/src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import routes from './routes'
+import store from './store'
+import VeeValidate from 'vee-validate';
+
+
+Vue.use(BootstrapVue)
+Vue.use(VueRouter)
+Vue.use(Vuex)
+Vue.use(VeeValidate);
+
+
+Vue.config.productionTip = false
+
+const router = new VueRouter({
+  routes // short for `routes: routes`
+})
+
+new Vue({
+  router,
+  store,
+  render: h => h(App),
+}).$mount('#app')
