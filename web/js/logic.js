b3ae0d25660040694c9c708b52b3f9190ce4ab6b
kymni <charles@akvo.org>
2019-08-27 17:20:22 +1200

    TASK-1: add logic to render individual todo using ajax

--- web/js/logic.js
+++ web/js/logic.js
@@ -23,3 +23,32 @@ function reloadTodos() {
         }
     });
 }
+
+$("#todo").ready(function() {
+    reloadTodo();
+});
+function reloadTodo() {
+    $.ajax({ 
+        type: 'GET',
+        url: $("#todo").data('id'), 
+        contentType: 'application/json',
+        dataType: 'json',
+        success: function( data ) {
+            if ('id' in data) {
+                let content;
+                console.log(data.completed);
+                content += '<tr><td>Item number</td><td>'+data.id+'</td> </tr>'
+                    +'<tr><td>User</td><td>'+data.user_id+'</td></tr>'
+                    +'<tr><td>Description</td><td>'+data.description+'</td></tr>'
+                    +'<tr><td>Complete</td><td>'
+                    +'<input type="checkbox" class="form-check-input" id="completed" data-id="'+data.id+'" '+(data.completed === '1' ? 'checked' : '')+'>' //check if todo is completed
+                    +'</td></tr>'
+                    +'<tr><td></td><td>'
+                    +'<form method="post" action="todo/delete/"'+data.id+'">'
+                    +'<button type="submit" class="btn btn-xs btn-danger"><span class="glyphicon glyphicon-remove glyphicon-white"></span></button>'
+                    +'</form></td></tr>';
+                $('#todo').html(content);
+            }
+        }
+    });
+}
