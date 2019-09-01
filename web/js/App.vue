95cc37580302456a7a861de04ce2359abe3dd703
Neo <neoye911@gmail.com>
2019-09-01 21:40:34 +1200

    add reusable button component

--- web/js/App.vue
+++ web/js/App.vue
@@ -1,9 +1,16 @@
 <template>
-    <h1>neo test</h1>
+    <div>
+        <h1>neo test</h1>
+        <t-button class="text-primary" primary loading label="read more"></t-button>
+    </div>
 </template>
 <script>
+import TButton from "./components/commons/TButton.vue"
 export default {
   name: "app",
+  components: {
+    TButton
+  },
   beforeMount() {
     // get the todos list data from the twig template
     const id = "vue-todos"
---
+++ web/js/components/commons/TButton.vue
@@ -0,0 +1,108 @@
+<template>
+  <button @click="buttonClick($event)" :class="['button', {'button--no-hover': loading}]">
+    <t-spinner class="button__spinner" v-if="loading"></t-spinner>
+    <div :class="['inline-block', 'pointer-events-none', {'button__text--invisible': loading}]">{{label}}</div>
+  </button>
+</template>
+
+<script>
+import TSpinner from './TSpinner';
+  export default {
+    name: 'TButton',
+    components: {
+      TSpinner
+    },
+    props: {
+      label: {
+        type: String,
+        default: ''
+      },
+      // add loading state to avoid double click
+      loading: {
+        type: Boolean,
+        default: false
+      }
+    },
+    methods: {
+      buttonClick(event) {
+        if(this.loading){ return; }
+        this.$emit('click', event);
+      },
+    }
+  }
+</script>
+
+<style scoped>
+.point-events-none {
+  pointer-events: none;
+}
+
+.inline-block {
+  display: inline-block;
+}
+
+.button {
+  font-size: 1.4rem;
+  font-weight: bold;
+  border-radius: .3rem;
+  text-transform: uppercase;
+  padding: 8px 10px;
+  transition: 0.3s;
+  cursor: pointer;
+}
+
+.button:focus {
+  outline: none;
+}
+
+.button[primary] {
+  background-color: var(--primary);
+  color: var(--white);
+}
+
+.button[primary]:hover:not(.button--no-hover) {
+  background-color: var(--primary-light);
+}
+
+.button[secondary] {
+  background-color: var(--secondary);
+  color: var(--white);
+}
+
+.button[borderless] {
+  position: relative;
+  padding: 0;
+}
+
+.button[borderless]::before {
+  background: currentColor;
+  content: '';
+  height: 2px;
+  width: 0%;
+  display: inline-block;
+  position: absolute;
+  left: 0;
+  bottom: -6px;
+  transition: width 0.3s;
+}
+
+.button[borderless]:hover:not(.button--no-hover).button[borderless]::before {
+   width: 100%;
+}
+
+.button__spinner {
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 1;
+  position: relative;
+}
+
+.button__text--invisible {
+  opacity: 0;
+  content: 'Sending...';
+  position: relative;
+  z-index: -1;
+  height: 0px;
+  display: block;
+}
+</style>
