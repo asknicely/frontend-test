fb58b556e8dd03dab71d21fb7571e98f2970b441
Neo <neoye911@gmail.com>
2019-09-01 18:00:51 +1200

    config vue loader and webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore.configureFilenames({
+    js: '[name].js',
+    css: '[name].css',
+    images: '[name].[hash:8].[ext]',
+    fonts: '[name].[hash:8].[ext]'
+}, 
+{
+    js: '[name].[chunkhash].js',
+    css: '[name].[contenthash].css',
+});
+Encore
+  .setOutputPath('web/build/')
+  .setPublicPath('/build')
+  .addEntry('app', './web/js/index.js')
+  .disableSingleRuntimeChunk()
+  .cleanupOutputBeforeBuild()
+  .enableSourceMaps(!Encore.isProduction())
+  .enableVersioning(Encore.isProduction())
+  .enableVueLoader()
+  .enableSassLoader();
+module.exports = Encore.getWebpackConfig();
