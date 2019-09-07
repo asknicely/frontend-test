416e374adb02eedc59fc044987169493d8669782
Neo <neoye911@gmail.com>
2019-09-07 17:52:38 +1200

    Task2: add axios and toast messager package

--- web/js/index.js
+++ web/js/index.js
@@ -1,5 +1,15 @@
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueToast from 'vue-toast-notification';
+import babelPolyfill from 'babel-polyfill'
 import App from './App'
+
+// Vue Instance Global Plugins
+Vue.use(VueAxios, axios)
+Vue.use(VueToast, {
+    position: 'top'
+})
 
 // when the template loaded then trigger Vue app instance
 document.addEventListener('DOMContentLoaded', function() {
