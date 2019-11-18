5093fa4a5b13de940ed9f7453a109ed13eaa3d3a
ignatiusnata <natawijaya1998@gmail.com>
2019-11-18 18:00:47 +1300

    Task-0 Converting login to secured react friendly API

--- src/controllers.php
+++ src/controllers.php
@@ -5,7 +5,6 @@ use Symfony\Component\HttpFoundation\Response;
 
 $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
     $twig->addGlobal('user', $app['session']->get('user'));
-
     return $twig;
 }));
 
@@ -17,28 +16,34 @@ $app->get('/', function () use ($app) {
 });
 
 
-$app->match('/login', function (Request $request) use ($app) {
-    $username = $request->get('username');
-    $password = $request->get('password');
-
-    if ($username) {
-        $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
-        $user = $app['db']->fetchAssoc($sql);
-
-        if ($user){
-            $app['session']->set('user', $user);
-            return $app->redirect('/todo');
-        }
-    }
-
-    return $app['twig']->render('login.html', array());
-});
+include 'routes/login.php';
+$app->mount('/', $user_login);
 
 
-$app->get('/logout', function () use ($app) {
-    $app['session']->set('user', null);
-    return $app->redirect('/');
-});
+
+
+// $app->match('/login', function (Request $request) use ($app) {
+//     $username = $request->get('username');
+//     $password = $request->get('password');
+
+//     if ($username) {
+//         $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
+//         $user = $app['db']->fetchAssoc($sql);
+
+//         if ($user){
+//             $app['session']->set('user', $user);
+//             return $app->redirect('/todo');
+//         }
+//     }
+
+//     return $app['twig']->render('login.html', array());
+// });
+
+
+// $app->get('/logout', function () use ($app) {
+//     $app['session']->set('user', null);
+//     return $app->redirect('/');
+// });
 
 
 $app->get('/todo/{id}', function ($id, Request $request) use ($app) {
---
+++ src/routes/login.php
@@ -0,0 +1,70 @@
+<?php
+
+use Firebase\JWT\JWT;
+use Symfony\Component\HttpFoundation\Request;
+
+$user_login = $app['controllers_factory'];
+$user_login->post('/login', function (Request $request) use ($app) {
+
+    // make sure these inputs are safe for SQL
+    $username = addslashes($request->get('username'));
+    $password = addslashes($request->get('password'));
+
+
+    // make sure the username and password 
+    // are filled first
+    
+    if (!$username || !$password) {
+        return $app->json([
+            'status' => 404,
+            'msg' => 'username and password are required',
+        ]);          
+    }
+
+
+    // since username and password 
+    // are included in the post request
+
+    if ($username && $password) {
+        
+        $sql = "SELECT * FROM users WHERE username = '$username' and password = '$password'";
+        $user = $app['db']->fetchAssoc($sql);
+
+        if ($user) {
+
+            // user is found in the db
+            // then create token & jwt
+
+            $token = array(
+                "username" => $username,
+                "password" => $password,
+                "login" => time(),
+                "expire" => time() + $app['config']['jwt']['expire']
+            );
+            
+            $jwt = JWT::encode($token, $app['config']['jwt']['secret']);
+            
+            // return jwt & token
+            
+            return $app->json([
+                'status' => 200,
+                'msg' => 'login is successful',
+                'token' => $jwt,
+            ]);
+
+        }
+        else {
+
+            // user is not found
+            // return an error
+
+            return $app->json([
+                'status' => 404,
+                'msg' => 'login is not successful',
+            ]);
+
+        }
+        
+    }
+
+});
